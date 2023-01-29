9a5a098425edba7a449c0e87879caa85e7caeef7
Petrushov Denis <bam411@yandex.ru>
2023-01-29 17:39:23 +0300

    Seminar 6

---
+++ Seminars/ex_036/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+int lineA = ReadInt("Введите сторону A: ");
+int lineB = ReadInt("Введите сторону B: ");
+int lineC = ReadInt("Введите сторону C: ");
+
+if (CheckSide(lineA, lineB, lineC) && CheckSide(lineB, lineA, lineC) && CheckSide(lineC, lineB, lineA))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+bool CheckSide(int checkSide, int a, int b)
+{
+    return checkSide < a + b;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Seminars/ex_037/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+int number = ReadInt("Введите десятичное число: ");
+string binaryNumber = "";
+int foundation = 2;
+while (number > 0)
+{
+    int divider = number % foundation;
+    binaryNumber = divider + binaryNumber;
+    number /= foundation;
+}
+Console.WriteLine(binaryNumber);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
